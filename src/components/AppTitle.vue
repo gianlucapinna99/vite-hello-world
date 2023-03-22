c190cec33ab97b0bfe8fb3c007d23dfd7703adc0
gianlucapinna99 <gianlucapinna99@gmail.com>
2023-03-22 18:04:17 +0100

    Vite finished

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,15 +1,17 @@
 <template>
-    <h1>La mia prima app con Vite!</h1>
+    <h1>LA MIA PRIMA APP CON VITE!</h1>
 </template>
 
 <style scoped>
 h1 {
+    margin: 0 auto;
     text-align: center;
     font-size: 40px;
     margin-top: 100px;
-    color: wheat;
     border: 1px solid grey;
-    border-radius: 15px;
-    background-color: grey;
+    border-radius: 30px;
+    background-color: white;
+    width: 50%;
+    padding: 10px 0px;
 }
 </style>
